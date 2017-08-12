2f595a61c8713fb5ad9302fc25d40aa8740f8e68
Esteban Echeverry <eecheverry@nubark.com>
2017-08-12 09:59:11 -0500

    Create main.py using aiohttp

--- authark/main.py
+++ authark/main.py
@@ -1,2 +1,16 @@
+from aiohttp import web
 
-import aiohttp
+
+async def handle(request: web.Request) -> web.Response:
+    name = request.match_info.get('name', 'Anonymous')
+    text = "Hello, " + name
+    return web.Response(text=text)
+
+
+app = web.Application()
+app.router.add_get('/', handle)
+app.router.add_get('/{name}', handle)
+
+
+if __name__ == '__main__':
+    web.run_app(app)
