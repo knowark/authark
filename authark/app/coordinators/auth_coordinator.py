7679390846da8677102b05dca4d8cab8ce9a62c1
Esteban Echeverry <eecheverry@nubark.com>
2017-08-26 16:46:59 -0500

    Add repository dependency to auth_coordinator

--- authark/app/coordinators/auth_coordinator.py
+++ authark/app/coordinators/auth_coordinator.py
@@ -3,8 +3,8 @@ from authark.app.repositories.user_repository import UserRepository
 
 class AuthCoordinator:
 
-    # def __init__(self, user_repository: UserRepository) -> None:
-    #     self.user_repository = user_repository
+    def __init__(self, user_repository: UserRepository) -> None:
+        self.user_repository = user_repository
 
     def authenticate(self, username: str, passwd: str) -> None:
         pass
--- tests/app/coordinator/test_auth_coordinator.py
+++ tests/app/coordinator/test_auth_coordinator.py
@@ -1,6 +1,25 @@
+from pytest import fixture
 from authark.app.coordinators.auth_coordinator import AuthCoordinator
+from authark.app.repositories.user_repository import UserRepository
+from authark.app.models.user import User
 
 
-def test_auth_coordinator() -> None:
-    auth_coordinator = AuthCoordinator()
+@fixture
+def mock_user_repository() -> UserRepository:
+    class MockUserRepository(UserRepository):
+
+        def get(self, username: str) -> User:
+            user = self.user_dict.get(username)
+            return user
+
+        def save(self, user: User) -> bool:
+            username = user.username
+            self.user_dict[username] = user
+            return True
+
+    return MockUserRepository()
+
+
+def test_auth_coordinator(mock_user_repository: UserRepository) -> None:
+    auth_coordinator = AuthCoordinator(mock_user_repository)
     assert hasattr(auth_coordinator, 'authenticate')
