c7f8158a03611364811830a043938705df6fea0f
Esteban Echeverry <eecheverry@nubark.com>
2017-10-22 16:29:11 -0500

    Fix typing in web.main

--- authark/infra/web/main.py
+++ authark/infra/web/main.py
@@ -1,8 +1,9 @@
+from typing import Dict
 from flask import Flask
 import authark.infra.web.routes as routes
 
 
-def main(config: dict = None) -> Flask:
+def main(config: Dict[str, str] = None) -> Flask:
 
     app = Flask(__name__)
 
