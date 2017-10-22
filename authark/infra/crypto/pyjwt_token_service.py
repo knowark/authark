9ab85037f97ef5702b6a350ed58bd37269645ab6
Esteban Echeverry <eecheverry@nubark.com>
2017-10-22 12:44:19 -0500

    Create pyjwt generate token implementation

--- authark/infra/crypto/pyjwt_token_service.py
+++ authark/infra/crypto/pyjwt_token_service.py
@@ -1,3 +1,4 @@
+from typing import Dict
 import jwt
 from authark.app.services.token_service import TokenService
 from authark.app.models.token import Token
@@ -5,7 +6,12 @@ from authark.app.models.token import Token
 
 class PyJWTTokenService(TokenService):
 
-    def generate_token(self) -> Token:
-        value = "todo"
+    def __init__(self, secret: str, algorithm: str) -> None:
+        self.secret = secret
+        self.algorithm = algorithm
+
+    def generate_token(self, payload: Dict[str, str]) -> Token:
+        payload = payload or {}
+        value = jwt.encode(payload, self.secret, algorithm=self.algorithm)
         token = Token(value)
         return token
--- tests/infra/services/test_pyjwt_token_service.py
+++ tests/infra/services/test_pyjwt_token_service.py
@@ -1,3 +1,4 @@
+import jwt
 from pytest import fixture
 from authark.app.models.token import Token
 from authark.app.services.token_service import TokenService
@@ -6,3 +7,21 @@ from authark.infra.crypto.pyjwt_token_service import PyJWTTokenService
 
 def test_pyjwt_token_service_implementation() -> None:
     assert issubclass(PyJWTTokenService, TokenService)
+
+
+def test_pyjwt_token_service_generate_token_with_payload() -> None:
+    secret = 'ABCDE12345'
+    algorithm = 'HS256'
+    payload = {'user': "Pepe", 'email': "pepe@gmail.com"}
+
+    pyjwt_service = PyJWTTokenService(secret=secret, algorithm=algorithm)
+    token = pyjwt_service.generate_token(payload)
+    value = token.value
+    print("--->>>", value)
+
+    decoded_payload = jwt.decode(value, secret, algorithms=[algorithm])
+    print("|||||--->>>", decoded_payload)
+
+    assert isinstance(token, Token)
+    assert value
+    assert payload == decoded_payload
