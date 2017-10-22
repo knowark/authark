7aaa803315e89d6f485d392196c837bccbe3fa51
Esteban Echeverry <eecheverry@nubark.com>
2017-10-22 13:54:04 -0500

    Require payload in application definition

--- authark/app/services/token_service.py
+++ authark/app/services/token_service.py
@@ -1,8 +1,9 @@
+from typing import Dict
 from abc import ABC, abstractmethod
 from authark.app.models.token import Token
 
 
 class TokenService(ABC):
     @abstractmethod
-    def generate_token(self) -> Token:
+    def generate_token(self, payload: Dict[str, str] = None) -> Token:
         ...
--- tests/app/services/test_token_service.py
+++ tests/app/services/test_token_service.py
@@ -1,6 +1,10 @@
+from inspect import signature
 from authark.app.services.token_service import TokenService
 
 
 def test_token_service() -> None:
     methods = TokenService.__abstractmethods__
     assert 'generate_token' in methods
+
+    sig = signature(TokenService.generate_token)
+    assert sig.parameters.get('payload')
