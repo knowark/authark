d00b4a0fdf408968f428dfb044a8449ee7008457
Esteban Echeverry <eecheverry@nubark.com>
2017-10-22 17:12:12 -0500

    Create auth post endpoint

--- authark/infra/web/resources/auth_resource.py
+++ authark/infra/web/resources/auth_resource.py
@@ -1,4 +1,5 @@
-from typing import Any
+from typing import Any, Dict, Tuple
+from flask import request
 from flask_restful import Resource
 
 
@@ -7,7 +8,7 @@ class AuthResource(Resource):
     def __init__(self, **kwargs: Any) -> None:
         self.auth_coordinator = kwargs['auth_coordinator']
 
-    def get(self) -> dict:
+    def get(self) -> Dict[str, str]:
         authenticated = self.auth_coordinator.authenticate(
             "Esteban", "Echeverry")
 
@@ -16,5 +17,14 @@ class AuthResource(Resource):
             'authenticated': authenticated
         }
 
-    def post(self) -> dict:
-        return {"is": "working"}
+    def post(self) -> Tuple[str, int]:
+        data = request.get_json()
+        username = data.get('username')
+        password = data.get('password')
+
+        try:
+            token = self.auth_coordinator.authenticate(username, password)
+        except Exception as e:
+            return '', 401
+
+        return token.value, 200
