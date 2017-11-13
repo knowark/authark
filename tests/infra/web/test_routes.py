675f22e0b9a5f93cb041c163a97d242300f4efd0
Esteban Echeverry <eecheverry@nubark.com>
2017-11-13 18:17:24 -0500

    Test authentication route

--- tests/infra/web/test_routes.py
+++ tests/infra/web/test_routes.py
@@ -1,15 +1,46 @@
 import json
 from pytest import fixture
 from flask import Flask
+from authark.app.models.user import User
+from authark.infra.web.registry import Registry
 from authark.infra.web.main import main
+
+from authark.app.coordinators.auth_coordinator import AuthCoordinator
+from authark.infra.db.memory_user_repository import MemoryUserRepository
+from authark.infra.crypto.pyjwt_token_service import PyJWTTokenService
+
+
+class MockRegistry(Registry):
+
+    def __init__(self) -> None:
+        user_repository = MemoryUserRepository()
+        user_repository.load({
+            'eecheverry': User(
+                "eecheverry",
+                "eecheverry@nubark.com",
+                "ABC1234"),
+            'mvivas': User(
+                "mvivas",
+                "mvivas@gmail.com",
+                "XYZ098"
+            )
+        })
+        token_service = PyJWTTokenService('TESTSECRET', 'HS256')
+        auth_coordinator = AuthCoordinator(user_repository, token_service)
+
+        self['auth_coordinator'] = auth_coordinator
 
 
 @fixture
 def app() -> Flask:
     """Create app testing client"""
-    app = main()
+
+    mock_registry = MockRegistry()
+
+    app = main(registry=mock_registry)
     app.testing = True
     app = app.test_client()
+
     return app
 
 
@@ -41,9 +72,10 @@ def test_auth_post_route_successful_authentication(app: Flask) -> None:
     response = app.post(
         '/auth',
         data=json.dumps(dict(
-            username="Esteban",
+            username="eecheverry",
             password="ABC1234"
         )),
         content_type='application/json')
-    expected_response = b''
-    assert expected_response in response.get_data()
+
+    assert response.status_code == 200
+    assert len(response.get_data()) > 0
