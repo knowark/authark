56a2cc6272dbf1a0c1693cb554adf7d6c851988e
Esteban Echeverry <eecheverry@nubark.com>
2017-11-13 16:21:57 -0500

    Use registry in web main function

--- authark/infra/web/main.py
+++ authark/infra/web/main.py
@@ -1,12 +1,13 @@
 from typing import Dict
 from flask import Flask
 import authark.infra.web.routes as routes
+from authark.infra.web.registry import Registry
 
 
 def main(config: Dict[str, str] = None) -> Flask:
 
     app = Flask(__name__)
-
-    routes.set_routes(app)
+    registry = Registry()
+    routes.set_routes(app, registry)
 
     return app
