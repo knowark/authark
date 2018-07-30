ca212085772d074ab0ef020f2abd4fdfcfc22ae7
Esteban Echeverry <eecheverry@nubark.com>
2018-07-30 18:12:39 -0500

    Remove default values from generate_token method

--- authark/application/services/token_service.py
+++ authark/application/services/token_service.py
@@ -6,10 +6,10 @@ from authark.application.models.token import Token
 
 class TokenService(ABC):
     @abstractmethod
-    def generate_token(self, payload: Dict[str, str] = None) -> Token:
+    def generate_token(self, payload: Dict[str, str]) -> Token:
         "Generate method to be implemented."
 
 
 class MemoryTokenService(TokenService):
-    def generate_token(self, payload: Dict[str, str] = None) -> Token:
+    def generate_token(self, payload: Dict[str, str]) -> Token:
         return Token(json.dumps(payload))
