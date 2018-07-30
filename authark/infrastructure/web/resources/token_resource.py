56e80c0e0ad80936be6806f3d2850803011b171c
Esteban Echeverry <eecheverry@nubark.com>
2018-07-30 17:13:40 -0500

    Rename auth an register resources to token and user

--- authark/infrastructure/web/resources/token_resource.py
+++ authark/infrastructure/web/resources/token_resource.py
@@ -3,7 +3,7 @@ from flask import request
 from flask_restful import Resource
 
 
-class RegisterResource(Resource):
+class TokenResource(Resource):
 
     def __init__(self, **kwargs: Any) -> None:
         self.auth_coordinator = kwargs['auth_coordinator']
