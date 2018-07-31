4a96fa79b4dc822eae75b4e286fa6de9ecd6f2fa
Esteban Echeverry <eecheverry@nubark.com>
2018-07-30 19:15:14 -0500

    Implement verify_password method in memory

--- tests/application/services/test_hash_service.py
+++ tests/application/services/test_hash_service.py
@@ -6,13 +6,14 @@ from authark.application.services.hash_service import (
 def test_hash_service() -> None:
     methods = HashService.__abstractmethods__
     assert 'generate_hash' in methods
+    assert 'verify_password' in methods
 
 
 def test_memory_hash_service_implementation() -> None:
     assert issubclass(MemoryHashService, HashService)
 
 
-def test_memory_hash_service_generate_hash_with_payload() -> None:
+def test_memory_hash_service_generate_hash() -> None:
     value = "SECRET_PASSWORD"
 
     hash_service = MemoryHashService()
@@ -20,3 +21,14 @@ def test_memory_hash_service_generate_hash_with_payload() -> None:
 
     assert isinstance(result, str)
     assert result == "HASHED: SECRET_PASSWORD"
+
+
+def test_memory_hash_service_verify_password() -> None:
+    hash_service = MemoryHashService()
+    password = "SECRET_PASSWORD"
+    hashed_password = "HASHED: SECRET_PASSWORD"
+
+    result = hash_service.verify_password(password, hashed_password)
+    assert result is True
+    result = hash_service.verify_password("WRONG_PASSWORD", hashed_password)
+    assert result is False
