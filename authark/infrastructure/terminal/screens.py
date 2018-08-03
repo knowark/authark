8a4049a68218a2db5b3a25d9958d579c9069ad7a
Esteban Echeverry <eecheverry@nubark.com>
2018-08-02 20:23:06 -0500

    Create Table widget

--- authark/infrastructure/terminal/screens.py
+++ authark/infrastructure/terminal/screens.py
@@ -1,4 +1,5 @@
 import urwid
+from authark.infrastructure.terminal.table import Table
 
 
 class Screen(urwid.WidgetWrap):
@@ -12,11 +13,19 @@ class Screen(urwid.WidgetWrap):
             "Press (", ("remove button", "R"), ") to remove a record. "
         ])
 
-        # Create the actual quote box
-        body_text = urwid.Text("Here comes the list!")
-        body_filler = urwid.Filler(body_text, valign='top', top=1, bottom=1)
-        body_padding = urwid.Padding(body_filler, left=1, right=1)
-        body = urwid.LineBox(body_padding)
+        # Create the contents table
+        # body_text = urwid.Text("Here comes the list!")
+        data = [
+            {'name': "Esteban", 'surname': "Echeverry", 'age': 29},
+            {'name': "Gabriel", 'surname': "Echeverry", 'age': 26},
+            {'name': "Valentina", 'surname': "Echeverry", 'age': 34}
+        ]
+        data = data * 5
+        body = Table(data)
+
+        # body_filler = urwid.Filler(body_text, valign='top', top=1, bottom=1)
+        # body_padding = urwid.Padding(body_filler, left=1, right=1)
+        # body = urwid.LineBox(body_padding)
 
         frame = urwid.Frame(header=header, body=body, footer=footer)
 
---
+++ authark/infrastructure/terminal/table.py
@@ -0,0 +1,58 @@
+import urwid
+from typing import List, Dict, Any
+
+
+class Table(urwid.WidgetWrap):
+
+    def __init__(self, data_list: List[Dict[str, Any]]):
+        header = self._build_header(data_list[0])
+        widget_list = [header]
+        for row_dict in data_list:
+            row_widget = TableRow(row_dict)
+            widget_list.append(row_widget)
+
+        widget = urwid.ListBox(urwid.SimpleFocusListWalker(widget_list))
+        widget.set_focus(1)
+        super().__init__(widget)
+
+    def _build_header(self, header_dict: Dict[str, Any]) -> urwid.Widget:
+        headers_list = [urwid.Pile([
+            urwid.Text(key.upper(), align='center'),
+            urwid.Text('-' * len(key), align='center')])
+            for key in header_dict.keys()]
+
+        return urwid.Columns(headers_list)
+
+    def keypress(self, size, key):
+        if key == 'home':
+            self._w.set_focus(1)
+            key = None
+        elif key == 'end':
+            self._w.set_focus(len(self._w.body) - 1)
+            key = None
+        elif key == 'up':
+            position = max(1, self._w.focus_position - 1)
+            self._w.set_focus(position)
+            key = None
+        elif key == 'down':
+            position = min(len(self._w.body) - 1, self._w.focus_position + 1)
+            self._w.set_focus(position)
+            key = None
+        return key
+
+
+class TableRow(urwid.WidgetWrap):
+
+    def __init__(self, row_dict: Dict[str, Any]):
+        items_list = []
+        for key, value in row_dict.items():
+            widget = urwid.Text(str(value), align='center')
+            items_list.append(widget)
+
+        widget = urwid.AttrMap(
+            urwid.Columns(items_list), None, focus_map='reversed')
+
+        super().__init__(widget)
+
+    # def keypress(self, size, key):
+    #     print("SUPERRRR!!!!  <<<<")
