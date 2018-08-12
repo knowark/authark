eafef168fa68563e31c32b2d3b87fc2cd4cffa33
Esteban Echeverry <eecheverry@nubark.com>
2018-08-12 16:15:35 -0500

    Add basic add user terminal form

--- authark/infrastructure/terminal/screens/users/users_actions.py
+++ authark/infrastructure/terminal/screens/users/users_actions.py
@@ -5,6 +5,8 @@ from authark.infrastructure.terminal.framework.screen import Screen
 class UsersAddScreen(Screen):
 
     def _build_widget(self) -> urwid.Widget:
+        self.auth_coordinator = self.env.context.registry['auth_coordinator']
+
         header = urwid.AttrMap(
             urwid.Text(self.name, align='center'), 'titlebar')
 
@@ -12,15 +14,22 @@ class UsersAddScreen(Screen):
             "Press (", ("back button", "B"), ") to go back. "
         ])
 
-        body = urwid.Filler(urwid.Text("Add Users"))
-        frame = urwid.Frame(header=header, body=body, footer=footer)
+        body = urwid.Pile([
+            urwid.Columns([
+                urwid.Text("Username: ", align='center'), urwid.Edit()]),
+            urwid.Columns([
+                urwid.Text("Email: ", align='center'), urwid.Edit()]),
+            urwid.Columns([
+                urwid.Text("Password: ", align='center'), urwid.Edit()]),
+        ])
+        body = urwid.Padding(urwid.Filler(body), align='center', width=80)
 
-        widget = frame
+        frame = urwid.Frame(header=header, body=body, footer=footer)
 
         widget = urwid.Overlay(
             frame, self.parent,
-            align='center', width=('relative', 80),
-            valign='middle', height=('relative', 80),
+            align='center', width=('relative', 50),
+            valign='middle', height=('relative', 50),
             min_width=20, min_height=9)
 
         return widget
