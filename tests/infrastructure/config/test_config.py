68cb5bd707d19d4a28714f2f07dffa31c773d262
Esteban Echeverry <eecheverry@nubark.com>
2018-10-01 22:31:44 -0500

    Test config module

---
+++ tests/infrastructure/config/test_config.py
@@ -0,0 +1,33 @@
+import os
+from json import JSONDecodeError
+from pytest import raises
+from authark.infrastructure.config.config import (
+    TrialConfig, DevelopmentConfig, ProductionConfig)
+
+
+def test_trial_config():
+    config = TrialConfig()
+    assert config['mode'] == 'TEST'
+
+
+def test_development_config():
+    config = DevelopmentConfig()
+    assert config['mode'] == 'DEV'
+
+
+def test_production_config(tmpdir):
+    os.chdir(str(tmpdir))
+    with open('config.json', 'w') as f:
+        f.write('{"database": {"type": "postgresql"}}')
+    config = ProductionConfig()
+    assert config['mode'] == 'PROD'
+    assert config['database'] == {'type': 'postgresql'}
+
+
+def test_production_config_wrong_format(tmpdir):
+    os.chdir(str(tmpdir))
+    with open('config.json', 'w') as f:
+        f.write('<Bad></Bad>')
+    config = ProductionConfig()
+    assert config['mode'] == 'PROD'
+    assert config['database']['type'] == 'json'
