0ff2fca476812f02934d67df52763733af74f9f4
eecheverry <eecheverry@nubark.com>
2018-10-02 19:14:21 -0500

    Remove obsolete if statements in memory repository search method

--- authark/application/repositories/memory_repository.py
+++ authark/application/repositories/memory_repository.py
@@ -23,15 +23,15 @@ class MemoryRepository(Repository, Generic[T]):
     def search(self, domain: QueryDomain, limit=0, offset=0) -> List[T]:
         items = []
         limit = int(limit) if limit > 0 else 100
+        offset = int(offset) if offset > 0 else 0
         filter_function = self.parser.parse(domain)
         for item in list(self.items.values()):
             if filter_function(item):
                 items.append(item)
 
-        if limit:
-            items = items[:limit]
-        if offset:
-            items = items[offset:]
+        items = items[:limit]
+        items = items[offset:]
+
         return items
 
     def remove(self, item: T) -> bool:
