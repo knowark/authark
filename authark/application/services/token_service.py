16d77cfd90836964a7e666d185d5b0c2d7cb51e5
eecheverry <eecheverry@nubark.com>
2018-10-16 16:57:45 -0500

    Add valid and renew method to token service

--- authark/application/services/token_service.py
+++ authark/application/services/token_service.py
@@ -9,7 +9,21 @@ class TokenService(ABC):
     def generate_token(self, payload: Dict[str, str]) -> Token:
         "Generate method to be implemented."
 
+    @abstractmethod
+    def valid(self, Token) -> bool:
+        "Valid method to be implemented."
+
+    @abstractmethod
+    def renew(self, Token) -> bool:
+        "Renew method to be implemented."
+
 
 class MemoryTokenService(TokenService):
     def generate_token(self, payload: Dict[str, Any]) -> Token:
         return Token(json.dumps(payload))
+
+    def valid(self, Token) -> bool:
+        return True
+
+    def renew(self, Token) -> bool:
+        return False
--- authark/infrastructure/crypto/pyjwt_token_service.py
+++ authark/infrastructure/crypto/pyjwt_token_service.py
@@ -7,10 +7,12 @@ from authark.application.models.token import Token
 
 class PyJWTTokenService(TokenService):
 
-    def __init__(self, secret: str, algorithm: str, lifetime: int) -> None:
+    def __init__(self, secret: str, algorithm: str, lifetime: int,
+                 threshold: int = None) -> None:
         self.secret = secret
         self.algorithm = algorithm
         self.lifetime = lifetime
+        self.threshold = threshold
 
     def generate_token(self, payload: Dict[str, Any]) -> Token:
         payload = payload or {}
@@ -18,3 +20,22 @@ class PyJWTTokenService(TokenService):
         value = jwt.encode(payload, self.secret, algorithm=self.algorithm)
         token = Token(str(value, 'utf-8'))
         return token
+
+    def valid(self, token: Token) -> bool:
+        decoded_payload = jwt.decode(
+            token.value, self.secret, algorithms=[self.algorithm])
+        return True
+
+    def renew(self, token: Token) -> bool:
+        if self.threshold is None:
+            return False
+
+        decoded_payload = jwt.decode(
+            token.value, self.secret, algorithms=[self.algorithm])
+        expiration = decoded_payload.get('exp', -1)
+        now = time()
+
+        if expiration - self.threshold <= now <= expiration:
+            return True
+
+        return False
