3599528d5f57c7d68ab16fb4b158467b0fc25668
eecheverry <eecheverry@nubark.com>
2018-11-23 09:12:47 -0500

    Test frameworks context

---
+++ tests/infrastructure/terminal/framework/test_context.py
@@ -0,0 +1,12 @@
+import urwid
+from pytest import fixture
+from authark.infrastructure.terminal.framework import Context
+
+
+def test_context_instantiation(environment):
+    class ConcreteContext(Context):
+        pass
+    context = ConcreteContext()
+    assert context is not None
+    assert isinstance(context.config, dict)
+    assert isinstance(context.registry, dict)
