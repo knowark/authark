d1cdd6ce17861fe486d95d8ef31eb2a6641d99d2
Esteban Echeverry <eecheverry@nubark.com>
2019-03-07 19:26:41 -0500

    Add apispec to project

--- authark/infrastructure/web/api.py
+++ authark/infrastructure/web/api.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from flask_restful import Api
-from flasgger import Swagger
+# from flasgger import Swagger
+from apispec import APISpec
 from ..resolver import Registry
-from .resources import UserResource, TokenResource
+from .resources import RootResource, UserResource, TokenResource
 
 
 def create_api(app: Flask, registry: Registry) -> None:
@@ -11,18 +12,29 @@ def create_api(app: Flask, registry: Registry) -> None:
     api = Api(app)
 
     # Swagger
-    Swagger(app, template_file="api.yml", config={
-        "specs_route": "/",
-        "headers": [],
-        "specs": [{
-            "endpoint": 'apispec_1',
-            "route": '/apispec_1.json',
-            "rule_filter": lambda rule: True,
-            "model_filter": lambda tag: True,
-        }],
-        "static_url_path": "/flasgger_static",
-        "swagger_ui": True
-    })
+    # Swagger(app, template_file="api.yml", config={
+    #     "specs_route": "/",
+    #     "headers": [],
+    #     "specs": [{
+    #         "endpoint": 'apispec_1',
+    #         "route": '/apispec_1.json',
+    #         "rule_filter": lambda rule: True,
+    #         "model_filter": lambda tag: True,
+    #     }],
+    #     "static_url_path": "/flasgger_static",
+    #     "swagger_ui": True
+    # })
+
+    spec = APISpec(
+        title="Authark",
+        version="1.0.0",
+        openapi_version="3.0.2",
+        info=dict(description="Authentication and Authorization Server"))
+
+    registry['spec'] = spec
+
+    api.add_resource(
+        RootResource, '/', resource_class_kwargs=registry)
 
     # Tokens Resource
     api.add_resource(
--- authark/infrastructure/web/resources/__init__.py
+++ authark/infrastructure/web/resources/__init__.py
@@ -1,2 +1,24 @@
 from .token import TokenResource
 from .user import UserResource
+
+
+from typing import Any, Dict, Tuple
+from flask import request, render_template, make_response
+from flask_restful import Resource
+
+
+class RootResource(Resource):
+
+    def __init__(self, **kwargs: Any) -> None:
+        self.spec = kwargs.get('spec')
+
+    def get(self) -> str:
+        if 'api' in request.args:
+            return self.spec.to_dict()
+
+        template = render_template('index.html', url="/?api")
+        response = make_response(template, 200, {
+            'Content-Type': 'text/html'
+        })
+
+        return response
