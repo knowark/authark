fbc9b7a14979c0806c7ebe1c18d5eda08de30f3e
eecheverry <eecheverry@nubark.com>
2019-04-11 11:11:17 -0500

    Comment out session coordinator temporarily

--- authark/application/coordinators/session_coordinator.py
+++ authark/application/coordinators/session_coordinator.py
@@ -1,17 +1,17 @@
-import time
-import datetime
-from typing import Dict, Union, Any
-from ..services import TenantService
+# import time
+# import datetime
+# from typing import Dict, Union, Any
+# from ..services import TenantService
 
 
-class SessionCoordinator:
-    def __init__(self,
-                 # auth_service: AuthService,
-                 tenant_service: TenantService) -> None:
-        # self.auth_service = auth_service
-        self.tenant_service = tenant_service
+# class SessionCoordinator:
+#     def __init__(self,
+#                  # auth_service: AuthService,
+#                  tenant_service: TenantService) -> None:
+#         # self.auth_service = auth_service
+#         self.tenant_service = tenant_service
 
-    def initialize(self, auth_token: str) -> None:
-        pass
-        # token = Token(auth_token)
-        # return self.auth_service.setup(token)
+#     def initialize(self, auth_token: str) -> None:
+#         pass
+#         # token = Token(auth_token)
+#         # return self.auth_service.setup(token)
