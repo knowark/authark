9522f4af9d6da5a8fcbab3538ee7c1b804f12ca1
jjalvarez <jjalvarez@servagro.com.co>
2019-08-28 16:36:22 -0500

    Adding general api tests, rename test_routes to test_api

--- tests/infrastructure/web/test_api.py
+++ tests/infrastructure/web/test_api.py
@@ -174,6 +174,21 @@ def headers() -> dict:
                                       algorithm='HS256').decode('utf-8'))
     }
 
+# General tests
+
+
+def test_root_resource(app: Flask, headers: dict) -> None:
+    response = app.get('/', headers=headers)
+    data = str(response.data, 'utf-8')
+    assert data is not None
+
+
+def test_root_resource_request_none(app: Flask, headers: dict) -> None:
+    response = app.get('/?api', headers=headers)
+    data = str(response.data, 'utf-8')
+    assert data is not None
+
+
 # Tokens resource tests
 
 
