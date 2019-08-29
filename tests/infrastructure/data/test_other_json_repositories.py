a9c92f25cd85b76ef9567a14cbcaf1f9c7edd312
jjalvarez <jjalvarez@servagro.com.co>
2019-08-29 08:43:12 -0500

    Replacing mocks with real instances.

--- tests/infrastructure/data/test_other_json_repositories.py
+++ tests/infrastructure/data/test_other_json_repositories.py
@@ -1,61 +1,81 @@
+from pytest import fixture
 from unittest.mock import Mock
 from authark.infrastructure.data import (
     JsonCredentialRepository, JsonDominionRepository, JsonGrantRepository,
     JsonPermissionRepository, JsonPolicyRepository, JsonRankingRepository,
     JsonResourceRepository, JsonRoleRepository, JsonUserRepository)
+from authark.application.utilities import (
+    ExpressionParser, StandardTenantProvider)
 
-# TODO: replace mocks with real values.
+
+@fixture
+def expresion_parser():
+    return ExpressionParser()
 
 
-def test_json_credential_repository_instantiation():
+@fixture
+def tenant_provider():
+    return StandardTenantProvider()
+
+
+def test_json_credential_repository_instantiation(
+        expresion_parser, tenant_provider):
     json_credential_repository = JsonCredentialRepository(
-        Mock(), Mock(), Mock())
+        "", expresion_parser, tenant_provider)
     assert isinstance(json_credential_repository, JsonCredentialRepository)
 
 
-def test_json_dominion_repository_instantiation():
+def test_json_dominion_repository_instantiation(
+        expresion_parser, tenant_provider):
     json_dominion_repository = JsonDominionRepository(
         Mock(), Mock(), Mock())
     assert isinstance(json_dominion_repository, JsonDominionRepository)
 
 
-def test_json_grant_repository_instantiation():
+def test_json_grant_repository_instantiation(
+        expresion_parser, tenant_provider):
     json_grant_repository = JsonGrantRepository(
-        Mock(), Mock(), Mock())
+        "", expresion_parser, tenant_provider)
     assert isinstance(json_grant_repository, JsonGrantRepository)
 
 
-def test_json_permission_repository_instantiation():
+def test_json_permission_repository_instantiation(
+        expresion_parser, tenant_provider):
     json_permission_repository = JsonPermissionRepository(
-        Mock(), Mock(), Mock())
+        "", expresion_parser, tenant_provider)
     assert isinstance(json_permission_repository, JsonPermissionRepository)
 
 
-def test_json_policy_repository_instantiation():
+def test_json_policy_repository_instantiation(
+        expresion_parser, tenant_provider):
     json_policy_repository = JsonPolicyRepository(
-        Mock(), Mock(), Mock())
+        "", expresion_parser, tenant_provider)
     assert isinstance(json_policy_repository, JsonPolicyRepository)
 
 
-def test_json_ranking_repository_instantiation():
+def test_json_ranking_repository_instantiation(
+        expresion_parser, tenant_provider):
     json_ranking_repository = JsonRankingRepository(
-        Mock(), Mock(), Mock())
+        "", expresion_parser, tenant_provider)
     assert isinstance(json_ranking_repository, JsonRankingRepository)
 
 
-def test_json_resource_repository_instantiation():
+def test_json_resource_repository_instantiation(
+        expresion_parser, tenant_provider):
     json_resource_repository = JsonResourceRepository(
-        Mock(), Mock(), Mock())
+        "", expresion_parser, tenant_provider)
     assert isinstance(json_resource_repository, JsonResourceRepository)
 
 
-def test_json_role_repository_instantiation():
+def test_json_role_repository_instantiation(
+        expresion_parser, tenant_provider):
     json_role_repository = JsonRoleRepository(
-        Mock(), Mock(), Mock())
+        "", expresion_parser, tenant_provider)
     assert isinstance(json_role_repository, JsonRoleRepository)
 
 
-def test_json_user_repository_instantiation():
+def test_json_user_repository_instantiation(
+        expresion_parser, tenant_provider):
     json_user_repository = JsonUserRepository(
-        Mock(), Mock(), Mock())
+        "", expresion_parser, tenant_provider)
     assert isinstance(json_user_repository, JsonUserRepository)
