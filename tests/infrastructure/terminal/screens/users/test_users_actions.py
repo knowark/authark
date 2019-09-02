107ca059035ba1f5dca75c4dfa8fed07e69225f6
jjalvarez <jjalvarez@servagro.com.co>
2019-09-02 15:55:20 -0500

    Test users actions complete coverage.

--- tests/infrastructure/terminal/screens/users/test_users_actions.py
+++ tests/infrastructure/terminal/screens/users/test_users_actions.py
@@ -1,8 +1,11 @@
 import urwid
 from pytest import raises, fixture
 from authark.infrastructure.terminal.framework import Screen, Table
+from authark.infrastructure.terminal.screens.main_menu import MainMenu
 from authark.infrastructure.terminal.screens.users import (
     UsersScreen, UsersAddScreen, UsersDeleteScreen, UsersCredentialsScreen)
+from authark.infrastructure.terminal.screens.users.users_actions import (
+    UsersUpdateScreen)
 
 
 class MockParent(Screen):
@@ -26,6 +29,18 @@ def users_delete_screen(main):
 @fixture
 def users_credentials_screen(main):
     return UsersCredentialsScreen('USERS CREDENTIALS', main.env, MockParent())
+
+
+@fixture
+def users_update_screen(main):
+    users_screen = UsersScreen('USERS', main.env)
+    users_screen.pile.focus_position = 1
+    users_screen.keypress((0, 0), 'U')
+    users_update_screen = users_screen.env.holder.original_widget
+    users_update_screen.env.stack.pop()
+    users_update_screen.env.stack.append(MainMenu('MainMenu', main.env))
+    users_update_screen.env.stack.append(users_screen)
+    return users_update_screen
 
 
 def test_users_add_screen_instantiation(users_add_screen):
@@ -103,3 +118,29 @@ def test_users_credentials_screen_instantiation_no_parent(main):
     users_credentials_screen = UsersCredentialsScreen(
         'USERS CREDENTIALS', main.env, None)
     assert users_credentials_screen._build_widget() is None
+
+
+def test_users_update_screen_no_parent(main):
+    assert UsersUpdateScreen(
+        'UPDATE USER', UsersScreen('USERS UPDATE', main.env).env, None
+    )is not None
+
+
+def test_users_update_screen_instantiation(users_update_screen):
+    assert users_update_screen is not None
+
+
+def test_users_update_screen_keypress_enter(users_update_screen):
+    users_update_screen.password.edit_text = "MY_PASSWORD"
+    users_update_screen.keypress((0, 0), "meta enter")
+    auth_coordinator = users_update_screen.auth_coordinator
+    response = auth_coordinator.authenticate(
+        "eecheverry", "MY_PASSWORD", "ALL")
+    assert response["refresh_token"] is not None
+    assert response["access_token"] is not None
+
+
+def test_users_update_screen_keypress_left(users_update_screen):
+    users_update_screen.keypress((0, 0), "left")
+    assert isinstance(
+        users_update_screen.env.holder.original_widget, MainMenu)
