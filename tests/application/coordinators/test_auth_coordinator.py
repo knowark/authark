fae808648da38176e374c35e844029ad05c46e75
jjalvarezl <jjalvarezl@unicauca.edu.co>
2019-09-03 11:44:34 -0500

    Auth coordinator test improvement.

--- tests/application/coordinators/test_auth_coordinator.py
+++ tests/application/coordinators/test_auth_coordinator.py
@@ -231,8 +231,9 @@ def test_auth_coordinator_register_duplicated_username_error(
         user_dict = {"username": "mvp", "email": "mvp@gmail.com",
                      "password": "PASS4"}
         auth_coordinator.register(user_dict)
-        # user_dict['email'] = "mvp@gmail.com"
         user_dict['username'] = "mvp"
+        auth_coordinator.register(user_dict)
+        user_dict['email'] = "mvp@gmail.com"
         auth_coordinator.register(user_dict)
 
 
