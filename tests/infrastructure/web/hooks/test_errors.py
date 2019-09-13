fb247b6dec8bc3349f2353d60bec20fc505a0222
jjalvarezl <jjalvarezl@unicauca.edu.co>
2019-09-13 10:30:56 -0500

    Handler error coverage

--- tests/infrastructure/web/hooks/test_errors.py
+++ tests/infrastructure/web/hooks/test_errors.py
@@ -1,19 +1,27 @@
+from pytest import raises
 from flask import Flask, abort
 from authark.infrastructure.web.hooks.errors import register_error_handler
 
 
 def test_handler_error():
-    app = Flask(__name__)
+    with raises(Exception):
+        app = Flask(__name__)
 
-    app.config['TESTING'] = True
-    app.config['LIVESERVER_PORT'] = 0
+        app.config['TESTING'] = True
+        app.config['LIVESERVER_PORT'] = 0
 
-    @app.route("/test1")
-    def test1():
-        abort(400, "Test error handled")
+        @app.route("/test1")
+        def test1():
+            abort(400, "Test error handled")
 
-    register_error_handler(app)
+        @app.route("/test2")
+        def test2():
+            raise Exception("Not HTTPException type")
 
-    response = app.test_client().get("/test1")
+        register_error_handler(app)
 
-    assert response.status == '400 BAD REQUEST'
+        response = app.test_client().get("/test1")
+
+        assert response.status == '400 BAD REQUEST'
+
+        response = app.test_client().get("/test2")
