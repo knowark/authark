55735468fce66d60c80837f505a9398d83fe326f
Esteban Echeverry <eecheverry@nubark.com>
2020-05-07 19:15:50 -0500

    Update main entrypoint

--- authark/__main__.py
+++ authark/__main__.py
@@ -1,27 +1,29 @@
 """
 Authark entrypoint
 """
-
 import os
 import sys
 import asyncio
 import uvloop
+from json import loads
+from pathlib import Path
 from injectark import Injectark
-# from authark.infrastructure.config import build_config
-# from authark.infrastructure.factories import build_factory, build_strategy
+from .infrastructure.core import Config, PRODUCTION_CONFIG
+from .infrastructure.factories import factory_builder, strategy_builder
 from .infrastructure.presenters.shell import Shell
 
 
 async def main(args=None):
-    config_path = os.environ.get('AUTHARK_CONFIG', 'config.json')
-    # config = build_config('PROD', config_path)
+    config_path = Path(os.environ.get('AUTHARK_CONFIG', 'config.json'))
+    config = loads(config_path.read_text()) if config_path.is_file() else {}
+    config: Config = {**PRODUCTION_CONFIG, **config}
 
-    # factory = build_factory(config)
-    # strategy = build_strategy(config['strategies'], config['strategy'])
-    # injector = Injectark(strategy=strategy, factory=factory)
-    # injector['SetupSupplier'].setup()
+    strategy = strategy_builder.build(config['strategies'])
+    factory = factory_builder.build(config)
 
-    # await Cli(config, injector).run(args or [])
+    injector = Injectark(strategy, factory)
+
+    await Shell(config, injector).run(args or [])
 
 
 if __name__ == '__main__':
