1ff10798e90e26ea9c5439f9250a42122dd55433
jbuitron <jairnet5@gmail.com>
2020-05-13 20:48:01 -0500

    change refresh_token and access_token in schema

--- authark/infrastructure/presenters/rest/schemas/token.py
+++ authark/infrastructure/presenters/rest/schemas/token.py
@@ -5,11 +5,12 @@ class TokenRequestSchema(Schema):
     tenant = fields.Str(required=True, example="knowark")
     username = fields.Str(example="amlopez")
     password = fields.Str(example="secret")
-    refresh_token = fields.Str(example="eyJhbG.eyJzdWIiOiIx.MjM5MadQssw5c")
+    refresh_token = fields.Str(data_key='refreshToken',
+        example="eyJhbG.eyJzdWIiOiIx.MjM5MadQssw5c")
     client = fields.Str(example="data_server")
     dominion = fields.Str(example="platform_xyz")
 
 
 class TokenSchema(Schema):
-    refresh_token = fields.Str(example="eyJhbG.eyJzdWIiOiIx.MjM5MadQssw5c")
-    access_token = fields.Str(example="eyJhbG.kpvaG4gRMyfQ.SflKxwssw5cdfs")
+    refresh_token = fields.Str(data_key='refreshToken', example="eyJhbG.eyJzdWIiOiIx.MjM5MadQssw5c")
+    access_token = fields.Str(data_key='accessToken', example="eyJhbG.kpvaG4gRMyfQ.SflKxwssw5cdfs")
