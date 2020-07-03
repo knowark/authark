dd8a2855c7f3bfc5d8e1344674ffc591151656b4
Esteban Echeverry <eecheverry@knowark.com>
2020-07-03 11:36:44 -0500

    Update policy model definition

--- authark/application/domain/models/policy.py
+++ authark/application/domain/models/policy.py
@@ -1,10 +1,11 @@
 from .entity import Entity
 
+
 class Policy(Entity):
     def __init__(self, **attributes) -> None:
         super().__init__(**attributes)
         self.id = attributes.get('id', '')
         self.resource = attributes.get('resource', '')
         self.privilege = attributes.get('privilege', '')
-        self.role = attributes.get('role', '')
+        self.role_id = attributes.get('role_id', '')
         self.rule = attributes.get('rule', '')
--- tests/application/domain/models/test_policy.py
+++ tests/application/domain/models/test_policy.py
@@ -1,23 +1,25 @@
 from pytest import fixture
 from authark.application.domain.models import Policy
 
+
 @fixture
 def policy():
     return Policy(
-        id_ = "1",
-        resource = "Resource name",
-        privilege = "Privilege name",
-        role = "Role name",
-        rule = "Rule name",
+        id="1",
+        resource="Resource name",
+        privilege="Privilege name",
+        role_id="1",
+        rule="Rule name",
     )
+
 
 def test_policy_creation(policy):
     assert policy is not None
 
 
 def test_policy_attributes(policy):
-    # assert policy.id == "1"
+    assert policy.id == "1"
     assert policy.resource == "Resource name"
     assert policy.privilege == "Privilege name"
-    assert policy.role == "Role name"
+    assert policy.role_id == "1"
     assert policy.rule == "Rule name"
