35145122eed6b864a3fc6aa1f5f264e912877433
Esteban Echeverry <eecheverry@knowark.com>
2020-07-05 12:45:18 -0500

    Update policy rest schema

--- authark/presenters/rest/schemas/policy.py
+++ authark/presenters/rest/schemas/policy.py
@@ -10,5 +10,6 @@ class PolicySchema(EntitySchema):
         required=True, example="1a915516-3875-44f6-ae96-7eea2e7d7981")
     resource = fields.Str(required=True, example="Name resource")
     privilege = fields.Str(required=True, example="Name privilege")
-    role = fields.Str(required=True, example="Name role")
+    role_id = fields.Str(data_key='roleId', required=True,
+                         example="e9fa671b-1124-4c8e-9041-40c4d1986df2")
     restriction = fields.Str(required=True, example="Name restriction")
