93e27bdea422049f45aef2dce3126f951971a93f
Esteban Echeverry <eecheverry@knowark.com>
2020-07-07 15:29:49 -0500

    Cover roles screen and UsersSelectionModal

--- authark/presenters/console/screens/roles/roles.py
+++ authark/presenters/console/screens/roles/roles.py
@@ -1,3 +1,5 @@
+from authark.application.domain.models.entity import Entity
+from typing import Dict, Optional, Any
 from widark import (
     Frame, Listbox, Label, Entry,
     Event, Modal, Button, Spacer, Color)
@@ -43,7 +45,7 @@ class RolesScreen(Frame):
             self.modal = UsersSelectionModal(
                 self, injector=self.injector,
                 role=self.role,
-                proportion={'height': 0.90, 'width': 0.95},
+                proportion={'height': 0.95, 'width': 0.95},
                 done_command=self.on_modal_done).launch()
         else:
             await self.load()
@@ -179,76 +181,129 @@ class UsersSelectionModal(Modal):
         self.authark_informer = self.injector['AutharkInformer']
         self.management_manager = self.injector['ManagementManager']
         self.role = context['role']
-        self.focused = None
+        self.focused = {}
+        self.available_domain = []
+        self.chosen_domain = []
         return super().setup(**context) and self
 
     def build(self) -> None:
         super().build()
         self.modal = None
 
-        frame = Frame(
-            self, title=f'Users for role: {self.role["name"]}').title_style(
-                Color.SUCCESS()).style(border=[]).weight(3, 2)
-
+        available_frame = Frame(self, title='Available').weight(
+            5, 4).grid(0, 0)
+        self.available_search = Entry(
+            available_frame, command=self.on_available_search).listen(
+                'keydown', self.on_available_search, True)
         self.available = Listbox(
-            frame, command=self.on_select).grid(col=0)
+            available_frame, command=self.on_select).weight(9).grid(1)
 
+        switchers = Frame(self).title_style(
+            Color.SUCCESS()).style(border=[]).weight(5).grid(0, 1)
+
+        Button(switchers, content='\U000025B6', command=self.on_choose).style(
+            Color.SUCCESS(), border=[]).grid(0)
+        Button(switchers, content='\U000025C0', command=self.on_clear).style(
+            Color.SUCCESS(), border=[]).grid(1)
+        Button(switchers, content='\U000023E9',
+               command=self.on_choose_all).style(
+            Color.SUCCESS(), border=[]).grid(2)
+        Button(switchers, content='\U000023EA',
+               command=self.on_clear_all).style(
+            Color.SUCCESS(), border=[]).grid(3)
+
+        chosen_frame = Frame(self, title='Chosen').weight(
+            5, 4).grid(0, 2)
+        self.chosen_search = Entry(chosen_frame).listen(
+            'keydown', self.on_chosen_search, True)
         self.chosen = Listbox(
-            frame, command=self.on_select).grid(col=1)
-        # Label(frame, content='Name:').grid(0, 0)
-        # self.name = Entry(frame, content=self.role['name']).style(
-        #     border=[0]).grid(0, 1).weight(col=2)
-        # Label(frame, content='Description:').grid(1, 0)
-        # self.description = Entry(frame, content=self.role['description']).style(
-        #     border=[0]).grid(1, 1).weight(col=2)
-        # Label(frame, content='ID:').grid(2, 0)
-        # Label(frame, content=f'{self.role["id"]}').grid(2, 1)
+            chosen_frame, command=self.on_select).weight(9).grid(1)
 
-        # menu = Frame(self, title='Menu').grid(col=1)
-        # Button(menu, content='Policies',
-        #        command=self.on_policies).style(border=[0])
-        # Button(menu, content='Users',
-        #        command=self.on_users).grid(1).style(
-        #            Color.SUCCESS(), border=[0])
-
-        # actions = Frame(
-        #     self, title='Actions').title_style(
-        #         Color.WARNING()).grid(1).span(col=2)
-        # Button(actions, content='Delete', command=self.on_delete
-        #        ).style(Color.DANGER()).grid(0, 1)
-        # Spacer(actions).grid(0, 2).weight(col=2)
-        # Button(actions, content='Save', command=self.on_save
-        #        ).style(Color.SUCCESS()).grid(0, 3)
-        # Button(actions, content='Cancel', command=self.on_cancel
-        #        ).style(Color.WARNING()).grid(0, 4)
+        actions = Frame(
+            self, title='Actions').title_style(
+                Color.WARNING()).grid(1).span(col=3)
+        Label(actions, content=f'Role: {self.role["name"]}').grid(0, 2)
+        Spacer(actions).grid(0, 3).weight(col=1)
+        Button(actions, content='Save', command=self.on_save
+               ).style(Color.SUCCESS()).grid(0, 4)
+        Button(actions, content='Cancel', command=self.on_cancel
+               ).style(Color.WARNING()).grid(0, 5)
 
     async def on_select(self, event: Event) -> None:
         item = getattr(event.target.parent, 'item', None)
         event.target.focus()
+        self.focused = item if isinstance(item, dict) else None
+
+    async def on_available_search(self, event: Event) -> None:
+        if event.key == '\n':
+            event.stop = True
+            text = self.available_search.text.strip()
+            self.available_domain = [] if not text else [
+                '|',  ('name', 'ilike', f'%{text}%'),
+                ('email', 'ilike', f'%{text}%')]
+            await self.load()
+            self.available_search.focus()
+
+    async def on_chosen_search(self, event: Event) -> None:
+        if event.key == '\n':
+            event.stop = True
+            text = self.chosen_search.text.strip()
+            self.chosen_domain = [] if not text else [
+                '|',  ('name', 'ilike', f'%{text}%'),
+                ('email', 'ilike', f'%{text}%')]
+            await self.load()
+            self.chosen_search.focus()
 
     async def load(self) -> None:
-        # available = await self.authark_informer.search(
-        #     'user', [('', '=', self.dominion['id'])])
-
         current_user_ids = [ranking['user_id'] for ranking in
                             await self.authark_informer.search('ranking', [
                                 ('role_id', '=', self.role['id'])])]
-
         available_users = await self.authark_informer.search('user', [
-            '!', ('id', 'in', current_user_ids)])
+            '!', ('id', 'in', current_user_ids)] + self.available_domain)
+        chosen_users = await self.authark_informer.search('user', [
+            ('id', 'in', current_user_ids)] + self.chosen_domain)
 
         self.available.setup(data=available_users,
-                             fields=['id' 'name', 'email'], limit=20).connect()
+                             fields=['username', 'email'],
+                             limit=20).connect()
+        self.chosen.setup(data=chosen_users,
+                          fields=['username', 'email'],
+                          limit=20).connect()
 
-    # async def on_cancel(self, event: Event) -> None:
-    #     await self.done({'result': 'cancelled'})
+    async def on_choose(self, event: Event) -> None:
+        self._switch(self.focused, self.available, self.chosen)
 
-    # async def on_delete(self, event: Event) -> None:
-    #     await self.management_manager.remove_role([self.role['id']])
-    #     await self.done({'result': 'deleted'})
+    async def on_choose_all(self, event: Event) -> None:
+        for item in list(self.available.data):
+            self._switch(item, self.available, self.chosen)
 
-    # async def on_policies(self, event: Event) -> None:
-    #     await self.done({'result': 'policies'})
+    async def on_clear(self, event: Event) -> None:
+        self._switch(self.focused, self.chosen, self.available)
 
-    # async def on_users(self, event: Event) -> None:
-    #     await self.done({'result': 'users'})
+    async def on_clear_all(self, event: Event) -> None:
+        for item in list(self.chosen.data):
+            self._switch(item, self.chosen, self.available)
+
+    def _switch(self, item: Optional[Dict[str, Any]],
+                source: Listbox, target: Listbox) -> None:
+        if item and item not in target.data:
+            target.data.append(item)
+            target.connect()
+        if item and item in source.data:
+            source.data.remove(item)
+            source.connect()
+
+    async def on_save(self, event: Event) -> None:
+        removing_ranking_ids = [
+            ranking['id'] for ranking in await self.authark_informer.search(
+                'ranking', [('role_id', '=', self.role['id']),
+                            ('user_id', 'in', [user['id'] for user in
+                                               self.available.data])])]
+        await self.management_manager.deassign_role(removing_ranking_ids)
+        adding_rankings = [{'role_id': self.role['id'], 'user_id': user['id']}
+                           for user in self.chosen.data]
+        await self.management_manager.assign_role(adding_rankings)
+        await self.done({'result': 'saved'})
+
+    async def on_cancel(self, event: Event) -> None:
+        await self.done({'result': 'cancelled'})
--- tests/presenters/console/screens/roles/test_roles.py
+++ tests/presenters/console/screens/roles/test_roles.py
@@ -4,7 +4,7 @@ from types import MethodType
 from pytest import fixture, mark
 from widark import Event
 from authark.presenters.console.screens.roles.roles import (
-    RolesScreen, RoleDetailsModal, UsersSelectionModal)
+    RolesScreen, RoleDetailsModal, UsersSelectionModal, DominionsModal)
 
 pytestmark = mark.asyncio
 
@@ -22,6 +22,11 @@ def role_details_modal(root, injector):
 
 
 @fixture
+def dominions_modal(root, injector):
+    return DominionsModal(root, injector=injector)
+
+
+@fixture
 def users_selection_modal(root, injector):
     role = {'id': '1', 'name': 'admin', 'dominion_id': '1', 'description': ''}
     return UsersSelectionModal(root, injector=injector, role=role)
@@ -31,7 +36,7 @@ async def test_roles_instantiation_defaults(roles_screen):
     assert roles_screen.dominion == {}
 
 
-async def test_roles_modal_load(roles_screen):
+async def test_roles_screen_load(roles_screen):
     roles_screen.build()
     await roles_screen.load()
     await asyncio.sleep(0)
@@ -39,7 +44,7 @@ async def test_roles_modal_load(roles_screen):
     assert len(roles_screen.body.data) == 1
 
 
-async def test_roles_modal_on_modal_done(roles_screen):
+async def test_roles_screen_on_modal_done(roles_screen):
     event = Event('Custom', 'done', details={'result': 'default'})
     await roles_screen.on_modal_done(event)
     await asyncio.sleep(0)
@@ -48,7 +53,23 @@ async def test_roles_modal_on_modal_done(roles_screen):
     assert len(roles_screen.body.data) == 1
 
 
-async def test_roles_modal_on_body(roles_screen):
+async def test_roles_screen_on_dominion(roles_screen):
+    event = Event('Custom', 'done', details={'result': 'default'})
+    await roles_screen.on_dominion(event)
+    await asyncio.sleep(0)
+
+    assert type(roles_screen.modal).__name__ == 'DominionsModal'
+
+
+async def test_roles_screen_on_dominion_switch(roles_screen):
+    event = Event('Custom', 'done', details={'id': '1', 'name': 'Dominion'})
+    await roles_screen.on_dominion_switch(event)
+    await asyncio.sleep(0)
+
+    assert roles_screen.dominion == {'id': '1', 'name': 'Dominion'}
+
+
+async def test_roles_screen_on_body(roles_screen):
     def target(item_):
         class MockTarget:
             class parent:
@@ -70,7 +91,7 @@ async def test_roles_modal_on_body(roles_screen):
     assert roles_screen.modal is not None
 
 
-async def test_roles_modal_on_create(roles_screen):
+async def test_roles_screen_on_create(roles_screen):
     event = Event('Mouse', 'click')
     await roles_screen.on_create(event)
     await asyncio.sleep(0)
@@ -78,7 +99,7 @@ async def test_roles_modal_on_create(roles_screen):
         'name': '', 'description': '', 'dominion_id': None, 'id': ''}
 
 
-async def test_roles_modal_on_modal_done(roles_screen):
+async def test_roles_screen_on_modal_done(roles_screen):
     roles_screen.role = {'id': '1', 'name': 'admin',
                          'dominion_id': '1', 'description': ''}
     event = Event('Custom', 'done', details={'result': 'policies'})
@@ -100,7 +121,7 @@ async def test_roles_modal_on_modal_done(roles_screen):
     assert type(roles_screen.modal).__name__ == 'UsersSelectionModal'
 
 
-async def test_roles_modal_on_cancel(roles_screen):
+async def test_roles_screen_on_cancel(roles_screen):
     event = Event('Mouse', 'click')
     given_result = None
 
@@ -112,6 +133,31 @@ async def test_roles_modal_on_cancel(roles_screen):
 
     await roles_screen.on_cancel(event)
     assert given_result == {'result': 'cancelled'}
+
+
+async def test_dominions_modal_on_body(dominions_modal):
+    given_details = None
+
+    async def mock_done(self, details):
+        nonlocal given_details
+        given_details = details
+
+    dominions_modal.connect()
+    dominions_modal.done = MethodType(mock_done, dominions_modal)
+
+    def target(item_):
+        class MockTarget:
+            class parent:
+                item = item_
+        return MockTarget()
+
+    event = Event('Mouse', 'click')
+    event.target = target({'id': '1', 'name': 'Dominion'})
+
+    await dominions_modal.on_body(event)
+    await asyncio.sleep(0)
+
+    assert given_details == {'id': '1', 'name': 'Dominion'}
 
 
 async def test_role_details_modal_on_buttons(role_details_modal):
@@ -157,7 +203,7 @@ async def test_role_details_modal_on_save(role_details_modal):
                             'id': '1', 'name': 'admin'}]
 
 
-async def xtest_users_selection_modal_on_select(users_selection_modal):
+async def test_users_selection_modal_on_select(users_selection_modal):
     def target(item_):
         class MockTarget:
             class parent:
@@ -167,16 +213,145 @@ async def xtest_users_selection_modal_on_select(users_selection_modal):
                 pass
         return MockTarget()
 
-    users_selection_modal.build()
-    assert len(users_selection_modal.available.data) == 0
-    assert len(users_selection_modal.chosen.data) == 0
-
+    users_selection_modal.connect()
     event = Event('Mouse', 'click')
-    event.target = target({'id': '1', 'name': 'admin',
-                           'dominion_id': '1', 'description': ''})
+    event.target = target({'id': '1', 'username': 'eecheverry',
+                           'email': 'eecheverry@knowark.com1'})
 
     await users_selection_modal.on_select(event)
     await asyncio.sleep(0)
 
-    assert len(users_selection_modal.available.data) == 0
-    assert len(users_selection_modal.chosen.data) == 1
+    assert users_selection_modal.focused == {
+        'id': '1', 'username': 'eecheverry',
+        'email': 'eecheverry@knowark.com1'}
+
+
+async def test_users_selection_modal_on_available_search(users_selection_modal):
+    users_selection_modal.build()
+    users_selection_modal.available_search.setup(content='value').connect()
+
+    assert users_selection_modal.available_search.text == 'value'
+    assert users_selection_modal.available_domain == []
+
+    await users_selection_modal.on_available_search(
+        Event('Keyboard', 'keydown', key=''))
+    await users_selection_modal.on_available_search(
+        Event('Keyboard', 'keydown', key='\n'))
+
+    await asyncio.sleep(0)
+    assert users_selection_modal.available_domain == [
+        '|', ('name', 'ilike', '%value%'), ('email', 'ilike', '%value%')]
+
+
+async def test_users_selection_modal_on_chosen_search(users_selection_modal):
+    users_selection_modal.build()
+    users_selection_modal.chosen_search.setup(content='value').connect()
+
+    assert users_selection_modal.chosen_search.text == 'value'
+    assert users_selection_modal.chosen_domain == []
+
+    await users_selection_modal.on_chosen_search(
+        Event('Keyboard', 'keydown', key=''))
+    await users_selection_modal.on_chosen_search(
+        Event('Keyboard', 'keydown', key='\n'))
+
+    await asyncio.sleep(0)
+    assert users_selection_modal.chosen_domain == [
+        '|', ('name', 'ilike', '%value%'), ('email', 'ilike', '%value%')]
+
+
+async def test_users_selection_modal_on_choose(users_selection_modal):
+    users_selection_modal.build()
+    users_selection_modal.chosen_search.setup(content='value').connect()
+
+    await users_selection_modal.on_choose(Event('Mouse', 'click'))
+    assert users_selection_modal.available.data == []
+
+    item = {'id': '1', 'usename': 'Dummy'}
+    users_selection_modal.focused = item
+    users_selection_modal.available.data = [item]
+
+    await users_selection_modal.on_choose(Event('Mouse', 'click'))
+    await asyncio.sleep(0)
+
+    assert users_selection_modal.available.data == []
+    assert users_selection_modal.chosen.data == [item]
+
+
+async def test_users_selection_modal_on_choose_all(users_selection_modal):
+    users_selection_modal.build()
+    users_selection_modal.chosen_search.setup(content='value').connect()
+
+    item_1 = {'id': '1', 'usename': 'Dummy 1'}
+    item_2 = {'id': '2', 'usename': 'Dummy 2'}
+
+    users_selection_modal.available.data = [item_1, item_2]
+
+    await users_selection_modal.on_choose_all(Event('Mouse', 'click'))
+    await asyncio.sleep(0)
+
+    assert users_selection_modal.available.data == []
+    assert users_selection_modal.chosen.data == [item_1, item_2]
+
+
+async def test_users_selection_modal_on_clear(users_selection_modal):
+    users_selection_modal.build()
+    users_selection_modal.chosen_search.setup(content='value').connect()
+
+    item = {'id': '1', 'usename': 'Dummy'}
+    users_selection_modal.focused = item
+    users_selection_modal.chosen.data = [item]
+
+    await users_selection_modal.on_clear(Event('Mouse', 'click'))
+    await asyncio.sleep(0)
+
+    assert users_selection_modal.chosen.data == []
+    assert users_selection_modal.available.data == [item]
+
+
+async def test_users_selection_modal_on_clear_all(users_selection_modal):
+    users_selection_modal.build()
+    users_selection_modal.chosen_search.setup(content='value').connect()
+
+    item_1 = {'id': '1', 'usename': 'Dummy 1'}
+    item_2 = {'id': '2', 'usename': 'Dummy 2'}
+    users_selection_modal.chosen.data = [item_1, item_2]
+
+    await users_selection_modal.on_clear_all(Event('Mouse', 'click'))
+    await asyncio.sleep(0)
+
+    assert users_selection_modal.chosen.data == []
+    assert users_selection_modal.available.data == [item_1, item_2]
+
+
+async def test_users_selection_modal_on_cancel(users_selection_modal):
+    event = Event('Mouse', 'click')
+    given_result = None
+
+    async def mock_done(self, result):
+        nonlocal given_result
+        given_result = result
+
+    users_selection_modal.done = MethodType(mock_done, users_selection_modal)
+
+    await users_selection_modal.on_cancel(event)
+    assert given_result == {'result': 'cancelled'}
+
+
+async def test_users_selection_modal_on_save(users_selection_modal):
+    event = Event('Mouse', 'click')
+
+    given_ranking_ids = None
+
+    async def deassign_role(self, ranking_ids):
+        nonlocal given_ranking_ids
+        given_ranking_ids = ranking_ids
+
+    users_selection_modal.build()
+    users_selection_modal.management_manager.deassign_role = MethodType(
+        deassign_role, users_selection_modal)
+
+    await users_selection_modal.on_save(event)
+    await asyncio.sleep(0)
+
+    assert given_ranking_ids == []
