2ccedd553c2328efba3401ebfaa3966fa123897d
eecheverry <eecheverry@knowark.com>
2020-09-11 16:59:05 -0500

    Make user schema have all its fields optional

--- authark/presenters/rest/schemas/user.py
+++ authark/presenters/rest/schemas/user.py
@@ -6,9 +6,8 @@ class UserSchema(EntitySchema):
     class Meta:
         unknown = EXCLUDE
 
-    tenant = fields.Str(required=True, load_only=True, example="knowark")
-    password = fields.Str(required=True, load_only=True, example="secret")
-    username = fields.Str(example="jarango")
-    email = fields.Str(example="jarango@ops.servagro.com.co")
     name = fields.Str(example="Jaime Arango")
+    email = fields.Str(example="jarango@ops.servagro.com.co")
+    username = fields.Str(example="jarango")
+    password = fields.Str(load_only=True, example="secret")
     attributes = fields.Mapping()
