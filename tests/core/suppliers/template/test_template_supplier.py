1d22bdb6566d80c014eec5920ceb3c801117970c
Esteban Echeverry <eecheverry@knowark.com>
2021-03-02 12:40:50 -0500

    Implement JinjaTemplateSupplier

--- tests/core/suppliers/template/test_template_supplier.py
+++ tests/core/suppliers/template/test_template_supplier.py
@@ -1,4 +1,5 @@
-from authark.core import TemplateSupplier, MemoryTemplateSupplier
+from authark.core import (
+    TemplateSupplier, MemoryTemplateSupplier, JinjaTemplateSupplier)
 
 
 def test_template_supplier_methods() -> None:
@@ -12,3 +13,38 @@ def test_template_supplier_render() -> None:
     result = template_supplier.render('activation.html', {'token': 'ABC123'})
 
     assert result == "activation.html: {'token': 'ABC123'}"
+
+def test_jinja_template_supplier_render(tmp_path) -> None:
+    templates_dir = tmp_path / 'templates'
+    templates_dir.mkdir()
+    custom_template = templates_dir / 'custom.html'
+    custom_template.write_text(
+"""<!DOCTYPE html>
+<html>
+  <head>
+      <title>{{ site }}</title>
+  </head>
+  <body>
+    <h1>Hello</h1>
+    <p>{{ username }}</p>
+  </body>
+</html>""")
+
+    template_supplier = JinjaTemplateSupplier([templates_dir])
+
+    result = template_supplier.render('custom.html', {
+        'site': 'Knowark',
+        'username': 'grinberg'
+    })
+
+    assert result == (
+"""<!DOCTYPE html>
+<html>
+  <head>
+      <title>Knowark</title>
+  </head>
+  <body>
+    <h1>Hello</h1>
+    <p>grinberg</p>
+  </body>
+</html>""")
