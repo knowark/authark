1df9ccb2ba1093f34105c5fd7beab3349cd7cb77
Esteban Echeverry <eecheverry@knowark.com>
2021-03-03 17:40:59 -0500

    Move verify method to procedure manager

--- tests/application/managers/test_procedure_manager.py
+++ tests/application/managers/test_procedure_manager.py
@@ -26,3 +26,16 @@ async def test_procedure_manager_fulfill(procedure_manager):
         'token': '{"type": "reset", "tenant": "default", "uid": "3"}'
     }
 
+async def test_procedure_manager_verify(procedure_manager):
+    verification_dicts: RecordList = [{
+        "tenant": "default",
+        'token': '{"type": "activation", "tenant": "default", "uid": "1"}'
+    }]
+
+    await procedure_manager.verify(verification_dicts)
+
+    user_repository = procedure_manager.user_repository
+
+    [user] = await user_repository.search([('id', '=', '1')])
+
+    assert user.active is True
